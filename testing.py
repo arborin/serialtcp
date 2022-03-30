caf0167c53e194128f6801f0da37280a543bebc8
nkobaidze <nika.kobaidze@gmail.com>
2022-03-30 17:13:14 +0400

    [FIX] serial data string format

--- testing.py
+++ testing.py
@@ -1,14 +1,28 @@
-l = ['09.03.2022', '10:22:58', 'PG:', '11', 'TQ:', '3.81', 'Nm', 'AN:', '1215', 'Grad', 'IO']
+# STRING EXAMPLE  30.03.2022 12:15:53 PG:11 TQ:0.03 Nm AN:1861 Grad ABBRUCH
+#                 ['30.03.2022', '12:15:53', 'PG:11', 'TQ:0.03', 'Nm', 'AN:1861', 'Grad', 'ABBRUCH']
+# STRING EXAMPLE  30.03.2022 12:15:49 PG:11 TQ:3.49 Nm AN:10 Grad IO
+#                 ['30.03.2022', '12:15:49', 'PG:11', 'TQ:3.49', 'Nm', 'AN:10', 'Grad', 'IO']
+            
+# get keys and values will be next elements in list
 
-values = {"PG": '', "TQ": '', "AN": ''}
+l = "30.03.2022 12:15:53 PG:11 TQ:0.03 Nm AN:1861 Grad ABBRUCH"
 
-for key in values.keys():
-    try:
-        index = l.index(key+":")
-        values[key] = l[index+1]
-    except:
-        print("key not found")
+update_vals = l.split(" ")
+
+values = {}
+values['Date'] = update_vals[0]
+values['Time'] = update_vals[1]
 
 
+for data in update_vals:
+    if "PG:" in data:
+        values['PG'] = data.split(":")[-1]
+    elif "TQ:" in data:
+        values['TQ'] = data.split(":")[-1]
+    elif "AN:" in data:
+        values['AN'] = data.split(":")[-1]
+        
+        
 
 print(values)
+        
