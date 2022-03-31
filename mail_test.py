fb3e0ec1490567285b780e978267d81e05388fa6
nkobaidze <nika.kobaidze@gmail.com>
2022-03-31 22:15:17 +0400

    [ADD] ssl mail test

--- mail_test.py
+++ mail_test.py
@@ -1,22 +1,32 @@
-import smtplib
-import time
-import serial
-import socket
-import sys
-from pymongo import MongoClient
-from termcolor import colored
-from datetime import datetime
+import smtplib, ssl
+
 
 sender = "from@example.com"
 receiver = "to@example.com"
+user = "f69396842d193f"
+password = "c831385ef5eec6"
+host = "smtp.mailtrap.io"
+port = 2525
+ssl_port = 465
 
 message = f"""\
 Subject: Hi Mailtrap
 To: {receiver}
 From: {sender}
-
 This is a test e-mail message."""
 
-with smtplib.SMTP("smtp.mailtrap.io", 2525) as server:
-    server.login("f69396842d193f", "c831385ef5eec6")
+
+
+with smtplib.SMTP(host, port) as server:
+    server.login(user, password)
     server.sendmail(sender, receiver, message)
+    print('mail successfully sent')
+
+
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL(host, ssl_port, context=context) as server:
+    server.login(user, password)
+    server.sendmail(sender, receivers, message)
+    print('ssl mail successfully sent')
